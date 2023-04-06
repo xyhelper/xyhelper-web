dfb68cdd8f05cd29f2b4106e2fb8c80a99818035
Li Dong <cnlidong@live.cn>
2023-04-06 21:16:18 +0800

    v1.0.0发布

--- release.sh
+++ release.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
+# set -x
 
-# 生成版本号
+# 生成版本号 如 1.2.3
 if [ -z "$1" ]; then
     echo "请输入版本号"
     exit 1
 fi
 version=$1
 
+# 获取 Major Minor Patch
+major=$(echo $version | cut -d. -f1)
+minor=$(echo $version | cut -d. -f2)
+patch=$(echo $version | cut -d. -f3)
+
+echo "major: $major"
+echo "minor: $minor"
+echo "patch: $patch"
 
 docker login -u xyhelper
 
@@ -18,4 +27,7 @@ cd ..
 # docker build -t xyhelper/xyhelper-web:latest .
 # docker push xyhelper/xyhelper-web:latest
 
-docker buildx build -f Dockerfile.release --build-arg Version=$version --platform linux/amd64,linux/arm64,linux/arm/v7 -t xyhelper/xyhelper-web:latest --push .
+docker buildx build -f Dockerfile.release --build-arg VERSION=v$version --platform linux/amd64,linux/arm64,linux/arm/v7 -t xyhelper/xyhelper-web:latest --push .
+docker buildx build -f Dockerfile.release --build-arg VERSION=v$version --platform linux/amd64,linux/arm64,linux/arm/v7 -t xyhelper/xyhelper-web:$major --push .
+docker buildx build -f Dockerfile.release --build-arg VERSION=v$version --platform linux/amd64,linux/arm64,linux/arm/v7 -t xyhelper/xyhelper-web:$major.$minor --push .
+docker buildx build -f Dockerfile.release --build-arg VERSION=v$version --platform linux/amd64,linux/arm64,linux/arm/v7 -t xyhelper/xyhelper-web:$major.$minor.$patch --push .
